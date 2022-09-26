9af1c97a0891a72bf6682bd2bc36b681bcd7e293
AnesuMurakata <murac96@gmail.com>
2022-09-26 12:51:48 +0200

    db config debug

--- dbconfig.php
+++ dbconfig.php
@@ -3,19 +3,11 @@
     {
     function __construct()
     {
-        //$this->open($_ENV["PERSISTENT_STORAGE_DIR"] . '/combadd.sqlite');
-        $this->open('first.db');
+        $this->open($_ENV["PERSISTENT_STORAGE_DIR"] . 'test.sqlite');
+        //$this->open('first.db');
     }
     }
     $dbh = new MyDB();
-    // Create connection
-    // $dbh = new mysqli($_SERVER["DATABASE_URL"], '', '');
-
-    // // Check connection
-    // if ($dbh->connect_error) {
-    // die("Connection failed: " . $dbh->connect_error);
-    // }
-    // echo "Connected successfully";
     if(!$dbh){
     echo $dbh->lastErrorMsg();
     } else {
