e9dcab8adee0f958dd9c727fc105357262dd889e
AnesuMurakata <murac96@gmail.com>
2022-09-27 03:56:17 +0200

    add delete logic

--- app.php
+++ app.php
@@ -2,8 +2,8 @@
 
     if (isset($_GET['del'])) {
         $id = $_GET['del'];
-        $query = "DELETE FROM books WHERE id=$id";
-        $_SESSION['message'] = "Book deleted!"; 
+        $query = "DELETE FROM books WHERE rowid=$id";
+		$dbh->exec($query);
         header('location: index.php');
     }
 
@@ -14,7 +14,6 @@
 	
 		$query = "UPDATE books SET name='$name', author='$author' WHERE rowid=$id";
 		$dbh->exec($query);
-		$_SESSION['message'] = "Address updated!"; 
 		header('location: index.php');
 	}
 
@@ -25,57 +24,6 @@
         // Makes query with post data
         $query = "INSERT INTO books (name, author) VALUES ('$name', '$author')";
         $dbh->exec($query);
-        $_SESSION['message'] = "Book saved";
-        // header('location: index.php');
+        header('location: index.php');
 	}
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP SQLite</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<?php // Makes query with rowid
-$query = "SELECT * FROM books";
-
-$results = $dbh->query($query); ?>
-
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>Author</th>
-			<th colspan="2">Action</th>
-		</tr>
-	</thead>
-	
-	<?php while ($row = $results->fetchArray()) { ?>
-		<tr>
-			<td><?php echo $row['name']; ?></td>
-			<td><?php echo $row['author']; ?></td>
-			<td>
-				<a href="app.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-			</td>
-			<td>
-				<a href="app.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
-			</td>
-		</tr>
-	<?php } ?>
-</table>
-
-	<form method="post" action="app.php" >
-		<div class="input-group">
-			<label>Name</label>
-			<input type="text" name="name" value="">
-		</div>
-		<div class="input-group">
-			<label>Author</label>
-			<input type="text" name="author" value="">
-		</div>
-		<div class="input-group">
-			<button class="btn" type="submit" name="save" >Save</button>
-		</div>
-	</form>
-</body>
-</html>
--- dbconfig.php
+++ dbconfig.php
@@ -4,23 +4,13 @@
     function __construct()
     {
         $this->open($_ENV["PERSISTENT_STORAGE_DIR"] . '/combadd.sqlite');
-        //$this->open('first.db');
     }
     }
     $dbh = new MyDB();
-    // Create connection
-    // $dbh = new mysqli($_SERVER["DATABASE_URL"], '', '');
-
-    // // Check connection
-    // if ($dbh->connect_error) {
-    // die("Connection failed: " . $dbh->connect_error);
-    // }
-    // echo "Connected successfully";
     if(!$dbh){
     echo $dbh->lastErrorMsg();
     } else {
-        $query = "CREATE TABLE IF NOT EXISTS books (id INT PRIMARY KEY, name STRING, author STRING)";
+        $query = "CREATE TABLE IF NOT EXISTS books (name STRING, author STRING)";
         $dbh->exec($query);
-        //echo "Opened database successfully\n";
     }
 ?>
