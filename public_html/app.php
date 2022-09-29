9ea3db61c6223033194e887c6f17052fbcb7ca2d
Henry Shepherdson <henry@henryshepherdson.com>
2022-09-29 09:57:02 +0200

    WIP

--- public_html/app.php
+++ public_html/app.php
@@ -1,28 +1,34 @@
 <?php include "database/db_connect.php"; 
 
     if (isset($_GET['del'])) {
+
         $id = $_GET['del'];
         $query = "DELETE FROM books WHERE rowid=$id";
-		$dbh->exec($query);
+		$db->exec($query);
         header('location: index.php');
+
     }
 
 	if (isset($_POST['update'])) {
+
 		$id = $_POST['id'];
-		$name = $_POST['name'];
+		$book_title = $_POST['book_title'];
 		$author = $_POST['author'];
 	
-		$query = "UPDATE books SET name='$name', author='$author' WHERE rowid=$id";
-		$dbh->exec($query);
+		$query = "UPDATE books SET book_title='$book_title', author='$author' WHERE rowid=$id";
+		$db->exec($query);
 		header('location: index.php');
+
 	}
 
 	if (isset($_POST['save'])) {
-		$name = $_POST['name'];
+
+		$book_title = $_POST['book_title'];
 		$author = $_POST['author'];
 
         // Makes query with post data
-        $query = "INSERT INTO books (name, author) VALUES ('$name', '$author')";
+        $query = "INSERT INTO books (book_title, author) VALUES ('$book_title', '$author')";
         $db->exec($query);
         header('location: index.php');
+		
 	}
